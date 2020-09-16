4764b1e26205e022ba670faf62452096d6487482
Seongho Han <seonghohan1228@gmail.com>
2020-09-16 19:12:51 +0900

    File directory change in magnetic_field.py

--- magnetic_field.py
+++ magnetic_field.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
-with h5py.File('MEPD_SCI_20200717_0851_ORB_08795.h5', 'r') as hdf:
+with h5py.File('./data/MEPD_SCI_20200717_0851_ORB_08795.h5', 'r') as hdf:
     data = np.array(hdf['/MEPD_SCI/block2_values'])
 
 TAM = data[:, 0:8]
